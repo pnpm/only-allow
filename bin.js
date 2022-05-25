ba08f8c88940676b1371255ebbce216f5b5ca6f9
lockiechen <33082528+lockiechen@users.noreply.github.com>
2022-05-25 14:40:12 +0800

    add

--- bin.js
+++ bin.js
@@ -28,15 +28,15 @@ if (usedPM && usedPM.name !== wantedPM && !isInstallAsDependency) {
       console.log(boxen(`Use "pnpm install" for installation in this project.
 
 If you don't have pnpm, install it via "npm i -g pnpm".
-For more details, go to https://pnpm.js.org/, ${process.env}`, boxenOpts))
+For more details, go to https://pnpm.js.org/`, boxenOpts))
       break
     case 'yarn':
       console.log(boxen(`Use "yarn" for installation in this project.
 
 If you don't have Yarn, install it via "npm i -g yarn".
-For more details, go to https://yarnpkg.com/, ${process.env}`, boxenOpts))
+For more details, go to https://yarnpkg.com/`, boxenOpts))
       break
   }
-  
+
   process.exit(1)
 }
