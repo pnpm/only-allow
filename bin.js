0a2511eec63938f160ddfb213dc395e1462ddaba
lockiechen <33082528+lockiechen@users.noreply.github.com>
2022-05-25 10:42:13 +0800

    check is dependency

--- bin.js
+++ bin.js
@@ -13,7 +13,8 @@ if (wantedPM !== 'npm' && wantedPM !== 'cnpm' && wantedPM !== 'pnpm' && wantedPM
   process.exit(1)
 }
 const usedPM = whichPMRuns()
-if (usedPM && usedPM.name !== wantedPM) {
+const isInstallAsDependency = process.env.INIT_CWD.indexOf('node_modules') > -1;
+if (usedPM && usedPM.name !== wantedPM && isInstallAsDependency) {
   const boxenOpts = { borderColor: 'red', borderStyle: 'double', padding: 1 }
   switch (wantedPM) {
     case 'npm':
