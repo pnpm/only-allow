a2cbc8e62d0885f77c9393431c23740ec51be684
Javien Lee <kraccoon@kakao.com>
2023-09-26 18:13:58 +0900

    feat: support Bun (#20)

--- bin.js
+++ bin.js
@@ -14,12 +14,12 @@ function box(s) {
 
 const argv = process.argv.slice(2)
 if (argv.length === 0) {
-  console.log('Please specify the wanted package manager: only-allow <npm|cnpm|pnpm|yarn>')
+  console.log('Please specify the wanted package manager: only-allow <npm|cnpm|pnpm|yarn|bun>')
   process.exit(1)
 }
 const wantedPM = argv[0]
-if (wantedPM !== 'npm' && wantedPM !== 'cnpm' && wantedPM !== 'pnpm' && wantedPM !== 'yarn') {
-  console.log(`"${wantedPM}" is not a valid package manager. Available package managers are: npm, cnpm, pnpm, or yarn.`)
+if (wantedPM !== 'npm' && wantedPM !== 'cnpm' && wantedPM !== 'pnpm' && wantedPM !== 'yarn' && wantedPM !== 'bun') {
+  console.log(`"${wantedPM}" is not a valid package manager. Available package managers are: npm, cnpm, pnpm, yarn or bun.`)
   process.exit(1)
 }
 const usedPM = whichPMRuns()
@@ -44,6 +44,12 @@ For more details, go to https://pnpm.js.org/`))
 
 If you don't have Yarn, install it via "npm i -g yarn".
 For more details, go to https://yarnpkg.com/`))
+
+    case 'bun':
+      console.log(box(`Use "bun install" for installation in this project.
+
+If you don't have Bun, go to https://bun.sh/docs/installation and find installation method that suits your environment".`))
+
       break
   }
   process.exit(1)
